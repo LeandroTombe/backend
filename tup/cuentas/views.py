175818dc06d3876c54b8174d5ce6a10dcffc1fa0
Leandro Tombetta <leandro.tombetta87@gmail.com>
2024-07-30 11:07:56 -0300

    agregado de integracion continua

--- tup/cuentas/views.py
+++ tup/cuentas/views.py
@@ -58,23 +58,7 @@ class UserLoginView(APIView):
             'access': str(refresh.access_token),
         }, status=status.HTTP_200_OK)
 
-"""
 
-class RegisterUserView(GenericAPIView):
-    serializer_class=UserRegisterSerializer
-    
-    
-    def post(self, request):
-        serializer = UserRegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()  # Guarda el usuario y devuelve la instancia del usuario
-            return Response({
-                'email': user.email,
-                'nombre': user.nombre,
-                'apellido': user.apellido
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-"""
 
 class ImportarUsuariosAPIView(views.APIView):
     #permission_classes = [IsAdminUser]
